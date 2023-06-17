9570670b3d41b760c3300e8bfb994fce0d1e8b80
AlfonsoAcosta28 <109112791+AlfonsoAcosta28@users.noreply.github.com>
2023-06-16 20:41:25 -0600

    Ejercicio 1
    Solucion del ejercicio numero 1
    Descripción de Actividad
    
    Recrear el ejemplo del video 2 de la unidad 1, pero en lugar de la estructura para animales crearla para vehículos.
    
    Crear una solución nueva compuesta por 2 proyectos.
    
    Un proyecto de consola .NET Framework
    Una librería de clases de .NET Framework
    Agregar la referencia Nuget "NAudio" en la librería de clases
    Descargar de internet sonidos de:
    
    Tren
    Camión
    Automóvil

---
+++ 01_ObjectOrientedProgramming/E1/DotNetProjects'Structure/AudioManager/Automovil.cs
@@ -0,0 +1,34 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioManager
+{
+    public class Automovil : Vehiculo
+    {
+        public override void VehiculoSonido()
+        {
+            base.VehiculoSonido();
+            AudioFileReader audioFile = new AudioFileReader("Resoures/coche.wav");
+            WaveOutEvent waveOutEvent = new WaveOutEvent();
+
+            waveOutEvent.Init(audioFile);
+            waveOutEvent.Play();
+            while (waveOutEvent.PlaybackState == PlaybackState.Playing)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            waveOutEvent.Dispose();
+            audioFile.Dispose();
+        }
+
+        public override string ToString()
+        {
+            return "Auto";
+        }
+    }
+}
---
+++ 01_ObjectOrientedProgramming/E1/DotNetProjects'Structure/DotNetProjects'Structure/Program.cs
@@ -0,0 +1,33 @@
+﻿using AudioManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetProjects_Structure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehiculo vehiculo = null;
+            vehiculo = new Automovil();
+            vehiculo.VehiculoSonido();
+
+            vehiculo = new Avion();
+            vehiculo.VehiculoSonido();
+
+            vehiculo = new Camion();
+            vehiculo.VehiculoSonido();
+
+            vehiculo = new Moto();
+            vehiculo.VehiculoSonido();
+
+            vehiculo = new Tren();
+            vehiculo.VehiculoSonido();
+
+            Console.ReadKey();
+        }
+    }
+}
