10f5f42cc10b49951b96e30a777dcda7c3aee243
AlfonsoAcosta28 <109112791+AlfonsoAcosta28@users.noreply.github.com>
2023-06-16 18:01:58 -0600

    Tutorial 2
    Creacion de la carpeta tuturial 2, donde se creo un menu en consola para poder reporducir algunos sonidos de animales en un loop infinito

---
+++ 01_ObjectOrientedProgramming/T2/OOP.ConsoleExample/OOP.ConsoleExample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudioManager;
+
+namespace OOP.ConsoleExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+
+                Console.WriteLine("Type the animal");
+
+                string animalType = Console.ReadLine().ToLower();
+                Animal animal = null;
+                switch (animalType)
+                {
+                    case "cat":
+                        animal = new Cat();
+                        break;
+                    case "cow":
+                        animal = new Cow();
+                        break;
+                    case "dog":
+                        animal = new Dog();
+                        break;
+                    case "elephant":
+                        animal = new Elephant();
+                        break;
+                    case "pig":
+                        animal = new Pig();
+                        break;
+                    case "lion":
+                        animal = new Lion();
+                        break;
+                    default:
+                        Console.WriteLine("Animal not found!");
+                        System.Threading.Thread.Sleep(1000);
+                        break;
+                }
+
+                if (animal != null)
+                {
+                    animal.animalSound();
+                }
+
+            }
+
+        }
+    }
+}
