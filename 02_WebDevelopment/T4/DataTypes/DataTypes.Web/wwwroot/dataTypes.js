9742788437a774a4a5e567309092ec815a651d04
AlfonsoAcosta28 <109112791+AlfonsoAcosta28@users.noreply.github.com>
2023-06-19 16:25:02 -0600

    Tutorial 4
    Diferencias entre tipos de datos en C# y JavaScript

---
+++ 02_WebDevelopment/T4/DataTypes/DataTypes.Web/wwwroot/dataTypes.js
@@ -0,0 +1,16 @@
+﻿
+var element = document.getElementById("example")
+var textExample = "this is a text";
+var aNumer = 10.33;
+
+var complexObject = {
+    name : "Israel",
+    lastName: "Perez",
+    age : 19
+}
+var ul = "<ul>"
+for (var i = 0; i < textExample.length; i++) {
+    ul += ("<li>" + textExample[i] + "</li>")
+}
+ul += "</ul>"
+element.innerHTML = ul;
---
+++ 02_WebDevelopment/T4/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "This is a string";
+            int age = 35;
+            DateTime now = DateTime.Now;
+            double amount = 0;
+
+            string string2 = text + "a second string" + age + now + amount;
+
+            string string3 = string.Format("The age is {0}, the time is {1}, and the amount is {2}", age, now, amount);
+
+            string string4 = $"The age is {age}, the time is {now}, and the amount is {amount}";
+
+            char sampleChar = 'C';
+            char[] arrayChar = string4.ToCharArray();
+
+            /* for(int i = 0; i < arrayChar.Length; i++)
+             {
+                 Console.WriteLine(arrayChar[i]);    
+             }*/
+
+            amount = (double)10 / (double)3;
+
+            DateTime dataTime = new DateTime(2000, 1, 1);
+
+            TimeSpan timeSamp1 = now - dataTime;
+            //var otherText = "this is a text";
+
+
+            Console.WriteLine(timeSamp1.TotalDays);
+            Console.ReadKey();
+        }
+    }
+}
