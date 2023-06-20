ae02b5528282934b537cce986430ba3895a3da80
AlfonsoAcosta28 <109112791+AlfonsoAcosta28@users.noreply.github.com>
2023-06-19 20:20:02 -0600

    Ejercicio 1
    2.E1 Customizing bootstrap | Fecha Límite 29/06/2023
    
    Realizar las siguientes tareas sobre el proyecto de GymManager AspNetCore:
    
    Agregar un archivo CSS que contenga las clases para personalizar los estilos de Bootstrap.
    Descargar un logotipo de gimnasio (a gusto del ingeniero) de https://www.freepik.com/ e integrarlo a la izquierda del nombre GymManager en el encabezado. *(USE FLATICON)*
    Cambiar el color de fondo del encabezado.
    Cambiar el tipo de fuente del encabezado.
    La integración del nombre debe ser una vista parcial que se integre al Layout, de tal manera que se pueda ver el nombre en todas las vistas

---
+++ 02_WebDevelopment/E1/Customizing bootstrap/GymMananger.Web/Controllers/MembersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymMananger.Web.Controllers
+{
+    public class MembersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
