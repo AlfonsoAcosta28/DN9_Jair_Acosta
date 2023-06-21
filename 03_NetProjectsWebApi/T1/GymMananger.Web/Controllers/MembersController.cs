a28309c2eefac7aa7d5a6376a4886f15dbdf1dd9
AlfonsoAcosta28 <109112791+AlfonsoAcosta28@users.noreply.github.com>
2023-06-21 16:33:53 -0600

    Tutorial 1
    Loyouts

---
+++ 03_NetProjectsWebApi/T1/GymMananger.Web/Controllers/MembersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymMananger.Web.Controllers
+{
+    public class MembersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
---
+++ 03_NetProjectsWebApi/T1/GymMananger.Web/Models/City.cs
@@ -0,0 +1,8 @@
+﻿namespace GymMananger.Web.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+    }
+}
