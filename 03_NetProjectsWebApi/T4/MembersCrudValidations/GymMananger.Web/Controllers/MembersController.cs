81dd953c144342054b96432e07171a64a274fef2
AlfonsoAcosta28 <109112791+AlfonsoAcosta28@users.noreply.github.com>
2023-06-22 17:11:05 -0600

    Tutorial 4
    MembersCrudValidations

---
+++ 03_NetProjectsWebApi/T4/MembersCrudValidations/GymMananger.Web/Controllers/MembersController.cs
@@ -0,0 +1,64 @@
+﻿using GymManager.ApplicationServices.Members;
+using GymManager.Core.Members;
+using GymMananger.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymMananger.Web.Controllers
+{
+    public class MembersController : Controller
+    {
+        private readonly IMembersAppService _membersAppService;
+
+        public MembersController(IMembersAppService membersAppService) { 
+            this._membersAppService = membersAppService;
+        }
+
+        public IActionResult Index()
+        {
+            List<Member> members = _membersAppService.GetMembers();
+            MemberListViewModel viewModel = new MemberListViewModel();
+
+            viewModel.NewMembersCount = 2;
+            viewModel.Members = members;
+
+            return View(viewModel);
+        }
+
+        public IActionResult Edit(int memberId)
+        {
+            Member members = _membersAppService.GetMember(memberId);
+           
+
+            return View(members);
+        }
+        [HttpPost]
+        public IActionResult Edit(Member member)
+        {
+            _membersAppService.EditMember(member);
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Member member)
+        {
+            _membersAppService.AddMember(member);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int memberId)
+        {
+            _membersAppService.DeleteMember(memberId);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
---
+++ 03_NetProjectsWebApi/T4/MembersCrudValidations/GymMananger.Web/Program.cs
@@ -0,0 +1,53 @@
+using GymManager.ApplicationServices.Members;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var cultureInfo = new CultureInfo("es-MX");
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation(options =>
+{
+    options.FileProviders.Add(new PhysicalFileProvider(AppContext.BaseDirectory));
+    
+});
+
+builder.Services.AddTransient<IMembersAppService, MemberAppService>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(name: "default", pattern: "{controller=Modal}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Members}/{action=Index}/{id?}");
+    endpoints.MapControllerRoute(
+        name: "attendance",
+        pattern: "attendance/{action=MemberIn}/{id?}",
+        defaults: new { controller = "Attendance" });
+});
+
+app.Run();
