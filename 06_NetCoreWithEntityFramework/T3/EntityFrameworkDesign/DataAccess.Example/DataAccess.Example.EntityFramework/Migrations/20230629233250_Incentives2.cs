98830deef3ec37790625245650714050f76a97ca
AlfonsoAcosta28 <109112791+AlfonsoAcosta28@users.noreply.github.com>
2023-06-29 17:34:46 -0600

    Tutorial 3
    EntityFrameworkDesign

---
+++ 06_NetCoreWithEntityFramework/T3/EntityFrameworkDesign/DataAccess.Example/DataAccess.Example.EntityFramework/Migrations/20230629233250_Incentives2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Example.EntityFramework.Migrations
+{
+    public partial class Incentives2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Incentives",
+                type: "decimal(13,2)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Amount",
+                table: "Incentives",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
