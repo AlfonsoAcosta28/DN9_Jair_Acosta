3c2a3a8b73ba0a62b3f040c320eb56618449f89d
AlfonsoAcosta28 <109112791+AlfonsoAcosta28@users.noreply.github.com>
2023-07-12 17:11:17 -0600

    Tutorial 4
    Cors

---
+++ 07_CRUDNetTools/T4/Cors/WebApi.Cors.Example/WebApi.Cors.Example/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(op =>
+{
+    op.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyOrigin().AllowAnyMethod().WithHeaders("Authorization"));
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseCors("CorsPolicy");
+app.Run();
---
+++ 07_CRUDNetTools/T4/Cors/WebApi.Cors.Example/Webcores.Example.Client/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApi.Cors.Example;
+
+namespace Webcores.Example.Client.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public List<WeatherForecast> WeatherForecasts { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGetAsync()
+        {
+            System.Threading.Thread.Sleep(5000);
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://localhost:7021/WeatherForecast");
+
+            if (response.IsSuccessStatusCode) { 
+                var content = await response.Content.ReadAsStringAsync();
+                var result = Newtonsoft.Json.JsonConvert.DeserializeObject<List<WeatherForecast>>(content);
+                WeatherForecasts = result;
+            }
+        }
+    }
+}
